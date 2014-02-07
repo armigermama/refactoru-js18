237d7fc4c9faa61fcda92ab9dc45fc171a4842b3
Roni Chen <roni.chen@comcast.net>
2014-02-06 23:25:22 -0700

    part ii

--- main.js
+++ main.js
@@ -3,13 +3,8 @@ $(function(){
 	var source = $("#product-template").html();
 	var template = Handlebars.compile(source);
 
-	var productCompile = function(aryProducts){
-		for (i=0; i<aryProducts.length; i++){
-			// var produdctTemplate = template(aryProducts[i]);
-			console.log(template(aryProducts[i]));
-		}
-	};
+	$("#product-template").append($(template(productsData)));
 
-	productCompile(productsData.productsList);
+
 
 });
